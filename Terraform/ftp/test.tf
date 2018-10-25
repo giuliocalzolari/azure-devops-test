6b612052ad025901300f6d2c12e9a16e796990e5
tristanmetcalfe <tristan.metcalfe@cloudreach.com>
2018-10-25 12:21:15 +0200

    Provisioner added.

--- Terraform/ftp/test.tf
+++ Terraform/ftp/test.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  subscription_id = "f43108ec-f2eb-4cb0-a6b0-37d635f13dfd"
+}
+
 resource "azurerm_resource_group" "generic_rg" {
   name     = "tristan-automated-ftp-deployments-rg"
   location = "West Europe"
